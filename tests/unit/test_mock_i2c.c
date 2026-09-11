c5296abba28d13fd8abd77641f7ded7d6e78b937
Thejus Sanil <ps_devteam@ensaer.com>
2026-09-11 14:30:33 +0530

    fix(test): add static qualifier to test_mock_i2c test functions

--- tests/unit/test_mock_i2c.c
+++ tests/unit/test_mock_i2c.c
@@ -20,7 +20,7 @@ void tearDown(void) {
     mock_i2c_clear_faults();
 }
 
-void test_mock_i2c_single_register_readback(void) {
+static void test_mock_i2c_single_register_readback(void) {
     status_t status = mock_i2c_set_register(BME280_I2C_ADDR, BME280_REG_CHIP_ID, BME280_CHIP_ID_VALUE);
     TEST_ASSERT_EQUAL_INT(STATUS_OK, status);
 
@@ -32,7 +32,7 @@ void test_mock_i2c_single_register_readback(void) {
     TEST_ASSERT_EQUAL_HEX8(BME280_REG_CHIP_ID, mock_i2c_get_last_reg(BME280_I2C_ADDR));
 }
 
-void test_mock_i2c_burst_readback(void) {
+static void test_mock_i2c_burst_readback(void) {
     const uint8_t calib_data[24] = {
         0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
         0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F, 0x10,
@@ -48,7 +48,7 @@ void test_mock_i2c_burst_readback(void) {
     TEST_ASSERT_EQUAL_HEX8_ARRAY(calib_data, rx_buf, sizeof(calib_data));
 }
 
-void test_mock_i2c_driver_write_interception(void) {
+static void test_mock_i2c_driver_write_interception(void) {
     const uint8_t config_bytes[2] = {0xCE, 0x10};
 
     status_t status = i2c_bus_write(OPT3001_I2C_ADDR, 0x01U, config_bytes, sizeof(config_bytes), 100);
@@ -60,7 +60,7 @@ void test_mock_i2c_driver_write_interception(void) {
     TEST_ASSERT_EQUAL_HEX8(0x10U, mock_i2c_get_register(OPT3001_I2C_ADDR, 0x02U));
 }
 
-void test_mock_i2c_address_nack_fault(void) {
+static void test_mock_i2c_address_nack_fault(void) {
     mock_i2c_inject_fault(MOCK_I2C_FAULT_NACK_ADDR, 0);
 
     uint8_t dummy = 0;
@@ -74,7 +74,7 @@ void test_mock_i2c_address_nack_fault(void) {
     TEST_ASSERT_EQUAL_INT(STATUS_ERR_SENSOR_NO_RESPONSE, status);
 }
 
-void test_mock_i2c_timeout_fault(void) {
+static void test_mock_i2c_timeout_fault(void) {
     mock_i2c_inject_fault(MOCK_I2C_FAULT_TIMEOUT, 0);
 
     uint8_t dummy = 0;
@@ -82,7 +82,7 @@ void test_mock_i2c_timeout_fault(void) {
     TEST_ASSERT_EQUAL_INT(STATUS_ERR_TIMEOUT, status);
 }
 
-void test_mock_i2c_data_corruption_fault(void) {
+static void test_mock_i2c_data_corruption_fault(void) {
     (void)mock_i2c_set_register(BME280_I2C_ADDR, 0xD0U, 0xAAU);
     mock_i2c_inject_fault(MOCK_I2C_FAULT_CORRUPT_DATA, 0);
 
@@ -92,7 +92,7 @@ void test_mock_i2c_data_corruption_fault(void) {
     TEST_ASSERT_EQUAL_HEX8(0x55U, rx_byte); /* 0xAA ^ 0xFF = 0x55 */
 }
 
-void test_mock_i2c_delayed_fault_trigger(void) {
+static void test_mock_i2c_delayed_fault_trigger(void) {
     (void)mock_i2c_set_register(BME280_I2C_ADDR, 0xD0U, 0x60U);
     /* Fail on 3rd transaction */
     mock_i2c_inject_fault(MOCK_I2C_FAULT_TIMEOUT, 3);
@@ -111,7 +111,7 @@ void test_mock_i2c_delayed_fault_trigger(void) {
     TEST_ASSERT_EQUAL_INT(STATUS_ERR_TIMEOUT, status);
 }
 
-void test_mock_i2c_device_ready_check(void) {
+static void test_mock_i2c_device_ready_check(void) {
     /* Initially unconfigured */
     status_t status = i2c_bus_is_device_ready(0x22U, 100);
     TEST_ASSERT_EQUAL_INT(STATUS_ERR_SENSOR_NO_RESPONSE, status);
@@ -122,7 +122,7 @@ void test_mock_i2c_device_ready_check(void) {
     TEST_ASSERT_EQUAL_INT(STATUS_OK, status);
 }
 
-void test_mock_i2c_null_ptr_guard(void) {
+static void test_mock_i2c_null_ptr_guard(void) {
     status_t status = i2c_bus_read(BME280_I2C_ADDR, 0xD0U, NULL, 1, 100);
     TEST_ASSERT_EQUAL_INT(STATUS_ERR_NULL_PTR, status);
 
@@ -130,7 +130,7 @@ void test_mock_i2c_null_ptr_guard(void) {
     TEST_ASSERT_EQUAL_INT(STATUS_ERR_NULL_PTR, status);
 }
 
-void test_mock_i2c_reset_isolation(void) {
+static void test_mock_i2c_reset_isolation(void) {
     (void)mock_i2c_set_register(BME280_I2C_ADDR, 0xD0U, 0x60U);
     mock_i2c_reset();
 
